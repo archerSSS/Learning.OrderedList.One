0037e2b3de846ba9d0efdf7bb7c18284cd5518f5
archerSSS <archyrysss@gmail.com>
2019-05-12 20:53:32 +0300

    OrderedListLesson_v.0.1

---
+++ AlgoTest_1/UnitTest1.cs
@@ -0,0 +1,151 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using AlgorithmsDataStructures;
+
+namespace AlgoTest_1
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestAddAsc_1()
+        {
+            OrderedList<int> ord = new OrderedList<int>(true);
+
+            ord.Add(1);
+            ord.Add(2);
+            ord.Add(3);
+            ord.Add(4);
+
+            Assert.AreEqual(ord.head.value, 1);
+            Assert.AreEqual(ord.head.next.value, 2);
+            Assert.AreEqual(ord.head.next.next.value, 3);
+            Assert.AreEqual(ord.head.next.next.next.value, 4);
+            Assert.AreEqual(ord.head.next.next.next.next, null);
+            Assert.AreEqual(ord.tail.value, 4);
+            Assert.AreEqual(ord.tail.prev.value, 3);
+            Assert.AreEqual(ord.tail.prev.prev.prev.prev, null);
+        }
+
+
+        [TestMethod]
+        public void TestAddAsc_2()
+        {
+            OrderedList<int> ord = new OrderedList<int>(true);
+
+            ord.Add(1);
+            ord.Add(22);
+            ord.Add(3);
+            ord.Add(14);
+
+            Assert.AreEqual(ord.tail.value, 22);
+            Assert.AreEqual(ord.tail.prev.value, 14);
+            Assert.AreEqual(ord.head.value, 1);
+            Assert.AreEqual(ord.head.next.value, 3);
+        }
+
+
+        [TestMethod]
+        public void TestAddAsc_3()
+        {
+            OrderedList<int> ord = new OrderedList<int>(true);
+
+            ord.Add(4);
+            ord.Add(3);
+            ord.Add(2);
+            ord.Add(1);
+
+            Assert.AreEqual(ord.tail.value, 4);
+            Assert.AreEqual(ord.tail.prev.value, 3);
+            Assert.AreEqual(ord.head.value, 1);
+            Assert.AreEqual(ord.head.next.value, 2);
+        }
+
+
+        [TestMethod]
+        public void TestAddAsc_4()
+        {
+            OrderedList<int> ord = new OrderedList<int>(true);
+
+            ord.Add(-1);
+            ord.Add(0);
+            ord.Add(-2);
+            ord.Add(-3);
+
+            Assert.AreEqual(ord.tail.value, 0);
+            Assert.AreEqual(ord.tail.prev.value, -1);
+            Assert.AreEqual(ord.head.value, -3);
+            Assert.AreEqual(ord.head.next.value, -2);
+        }
+
+
+        [TestMethod]
+        public void TestAddDes_3()
+        {
+            OrderedList<int> ord = new OrderedList<int>(false);
+
+            ord.Add(4);
+            ord.Add(3);
+            ord.Add(2);
+            ord.Add(1);
+
+            Assert.AreEqual(ord.tail.value, 1);
+            Assert.AreEqual(ord.tail.prev.value, 2);
+            Assert.AreEqual(ord.head.value, 4);
+            Assert.AreEqual(ord.head.next.value, 3);
+        }
+
+
+        [TestMethod]
+        public void TestAddDes_4()
+        {
+            OrderedList<int> ord = new OrderedList<int>(false);
+
+            ord.Add(-1);
+            ord.Add(0);
+            ord.Add(-2);
+            ord.Add(-3);
+
+            Assert.AreEqual(ord.tail.value, -3);
+            Assert.AreEqual(ord.tail.prev.value, -2);
+            Assert.AreEqual(ord.head.value, 0);
+            Assert.AreEqual(ord.head.next.value, -1);
+        }
+
+
+        [TestMethod]
+        public void TestAddDes_5()
+        {
+            OrderedList<int> ord = new OrderedList<int>(false);
+
+            ord.Add(1);
+            ord.Add(22);
+            ord.Add(3);
+            ord.Add(14);
+
+            Assert.AreEqual(ord.head.value, 22);
+            Assert.AreEqual(ord.head.next.value, 14);
+            Assert.AreEqual(ord.tail.value, 1);
+            Assert.AreEqual(ord.tail.prev.value, 3);
+        }
+
+
+        [TestMethod]
+        public void TestAddDes_6()
+        {
+            OrderedList<int> ord = new OrderedList<int>(false);
+
+            ord.Add(5);
+            ord.Add(13);
+            ord.Add(3);
+            ord.Add(14);
+
+            Assert.AreEqual(ord.tail.value, 3);
+            Assert.AreEqual(ord.tail.prev.value, 5);
+            Assert.AreEqual(ord.tail.prev.next.value, 3);
+            Assert.AreEqual(ord.head.value, 14);
+            Assert.AreEqual(ord.head.next.value, 13);
+            Assert.AreEqual(ord.head.next.prev.value, 14);
+        }
+    }
+}
---
+++ AlgorithmsDataStructures/OrderedList.cs
@@ -0,0 +1,165 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace AlgorithmsDataStructures
+{
+
+    public class Node<T>
+    {
+        public T value;
+        public Node<T> next, prev;
+
+        public Node(T _value)
+        {
+            value = _value;
+            next = null;
+            prev = null;
+        }
+    }
+
+    public class OrderedList<T>
+    {
+        public Node<T> head, tail;
+        private bool _ascending;
+
+        public OrderedList(bool asc)
+        {
+            head = null;
+            tail = null;
+            _ascending = asc;
+        }
+
+        public int Compare(T v1, T v2)
+        {
+            int result = 0;
+            if (typeof(T) == typeof(String))
+            {
+                // версия для лексикографического сравнения строк
+            }
+            else
+            {
+                int n1 = 0;
+                int n2 = 0;
+                if (v1.GetType() == typeof(int) && v2.GetType() == typeof(int))
+                {
+                    n1 = Convert.ToInt32(v1);
+                    n2 = Convert.ToInt32(v2);
+                    if (n1 > n2) result = 1;
+                    else if (n1 < n2) result = -1;
+                }
+                // if (v1.GetHashCode() > v2.GetHashCode()) result = 1;
+                // else if (v1.GetHashCode() < v2.GetHashCode()) result = -1;
+                // универсальное сравнение
+            }
+
+            return result;
+            // -1 если v1 < v2
+            // 0 если v1 == v2
+            // +1 если v1 > v2
+        }
+
+        public void Add(T value)
+        {
+            if (head == null) { head = new Node<T>(value); tail = head; return; }
+            Node<T> node = head;
+            Node<T> new_node = new Node<T>(value);
+
+            int asc = 0;
+            if (_ascending) asc = 1;
+            else asc = -1;
+
+            while (node != null)
+            {
+                if (Compare(value, node.value) == -asc)
+                {
+                    if (node.prev == null)
+                    {
+                        new_node.next = node;
+                        node.prev = new_node;
+                        head = new_node;
+                    }
+                    else
+                    {
+                        node.prev.next = new_node;
+                        new_node.next = node;
+                        new_node.prev = node.prev;
+                        node.prev = new_node;
+
+                    }
+                    return;
+                }
+                node = node.next;
+            }
+            new_node.prev = tail;
+            new_node.prev.next = new_node;
+            tail = new_node;
+            // автоматическая вставка value 
+            //в нужную позицию
+        }
+
+        public Node<T> Find(T val)
+        {
+            return null; // здесь будет ваш код
+        }
+
+        public void Delete(T val)
+        {
+            if (head == null) return;
+            Node<T> node = head;
+            while (node != null)
+            {
+                if (node.value.Equals(val))
+                {
+                    if (node == head && node == tail)
+                    {
+                        head = null;
+                        tail = null;
+                    }
+                    else if (node.prev == null)
+                    {
+                        head = head.next;
+                        node.next.prev = node.prev;
+                    }
+                    else if (node.next == null)
+                    {
+                        tail = tail.prev;
+                        node.prev.next = node.next;
+                    }
+                    else
+                    {
+                        node.prev.next = node.next;
+                        node.next.prev = node.prev;
+                    }
+                }
+                node = node.next;
+            }
+
+            // здесь будет ваш код
+        }
+
+        public void Clear(bool asc)
+        {
+            _ascending = asc;
+            // здесь будет ваш код
+        }
+
+        public int Count()
+        {
+            return 0; // здесь будет ваш код подсчёта количества элементов в списке
+        }
+
+        List<Node<T>> GetAll() // выдать все элементы упорядоченного 
+                               // списка в виде стандартного списка
+        {
+            List<Node<T>> r = new List<Node<T>>();
+            Node<T> node = head;
+            while (node != null)
+            {
+                r.Add(node);
+                node = node.next;
+            }
+            return r;
+        }
+    }
+
+}
